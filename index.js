aac8b4f9dfa6e64fbf6376704822169898a22b59
Boby Robert <bobyrobert3@gmail.com>
2019-03-21 02:33:43 +0530

    Swapping using 2 variables
    Number swapping using two variables

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ module.exports = function (arr, cmp) {
   for (var i = 0, l = arr.length; i < l; i++) {
     for (var j = i; j > 0; j--) {
       if (cmp(arr[j], arr[j - 1]) < 0) {
-        temp = arr[j];
-        arr[j] = arr[j - 1];
-        arr[j - 1] = temp;
+        arr[j] = arr[j] + arr[j - 1];
+        arr[j - 1] = arr[j] - arr[j - 1];
+        arr[j] = arr[j] - arr[j - 1];
       }
     }
   }
